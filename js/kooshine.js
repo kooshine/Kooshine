c43cd0e7e1d5a8707f9c1f78222ba2f53081e51a
kooshine <18919027903@163.com>
2019-01-25 11:15:36 +0800

    javascript 0.1

---
+++ js/kooshine.js
@@ -0,0 +1,10 @@
+function code(){
+        alert("代码如诗");
+}
+function hardware(){
+    alert("树莓派再等等……")
+}
+function about(){
+    var id = document.getElementById('content');
+    console.log(id);
+}
